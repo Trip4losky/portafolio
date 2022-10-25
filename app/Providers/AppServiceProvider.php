71a27e09a48fe0f3bddd253231c7a4136e6c5df5
Trip4losky <agustinrallysport@gmail.com>
2022-10-25 10:15:23 +0100

    Edit public for work on Hostinger

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //Public folder name changed with public_html
-        $this->app->bind('path.public', function(){
-            return base_path().'/public_html';
-        });
+        //
     }
 
     /**
